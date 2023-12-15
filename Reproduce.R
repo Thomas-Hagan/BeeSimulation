3ed4c975e3b8510ad9039c801e7699849719a471
Thomas-Hagan <78055316+Thomas-Hagan@users.noreply.github.com>
2023-12-15 15:44:45 -0600

    Add files via upload

---
+++ Reproduce.R
@@ -0,0 +1,87 @@
+Reproduce <- function(Max_Swarms, GQL, Queens, Chance_Mutate, Queenless_Chance) {
+  
+  #Creates a bunch of names, creates a new data frame with columns equal to those number of names and then calls those columns those names
+  Column_Names = names(Queens)
+  New_Queens = data.frame(matrix(nrow = 0, ncol = length(Column_Names)))
+  New_Allele_Counter = 0
+  
+  for (i in 1:length(Queens$Position)) {
+    
+    #Creates a random number, with the average being the max number of swamrs multiplied by fitness
+    Num_Swarms = rpois(1, Max_Swarms*Queens$Fitness[i])
+    
+    #Starts making new colonies
+    if (Num_Swarms > 0) {
+      for (j in 1:Num_Swarms) {
+        
+        if (runif(1)<Queenless_Chance) {
+          Current_New_Queen <- Queens[i,]
+          Current_New_Queen$Age = GQL
+          Current_New_Queen = unlist(Current_New_Queen)
+          names(Current_New_Queen) = NULL
+          
+          if (length(Current_New_Queen)<length(New_Queens)) {
+            number0 = length(New_Queens) - length(Current_New_Queen)
+            Current_New_Queen = c(Current_New_Queen, rep(0, number0))
+          }
+          
+          New_Queens <- rbind(New_Queens, Current_New_Queen) 
+        } else {
+          #Takes mothers alleles
+          MotherAllele <- c(Queens$Allele_1[i], Queens$Allele_2[i])
+          
+          #Figures out how many alleles there are in the population and makes a new vector
+          Pop_Alleles = length(Queens) - 6
+          FatherAllele = c()
+          
+          #Takes fathers alleles
+          for (k in 1:Pop_Alleles) {
+            FatherAllele = c(FatherAllele, rep(k, Queens[i, k+6]))
+          }
+          
+          #Randomly takes one of the mothers alleles and on of the fathers, then binds this into the New_Queens data table at the same position
+          if (runif(1) <= Chance_Mutate) {
+            #This creates a new allele, it's value is equal to one more than the number of alleles
+            Allele1 <- length(Queens)-5+New_Allele_Counter
+            New_Allele_Counter = New_Allele_Counter + 1
+          } else {
+            Allele1 <- sample(MotherAllele, 1)
+          }
+          
+          if (isTRUE(length(FatherAllele) == 1)) {
+            Allele2 = FatherAllele
+          } else {
+            Allele2 <- sample(FatherAllele, 1)
+          }
+          
+          Current_New_Queen <- c(Queens$Position[i], rep(0, 3), Allele1, Allele2, rep(0, Pop_Alleles))
+          New_Queens <- rbind(New_Queens, Current_New_Queen) 
+        }
+      }
+    }
+  }
+  
+  colnames(New_Queens) = Column_Names
+  
+  #Creates a new column for alleles if the queen has mutated
+  if (max(New_Queens$Allele_1) > Pop_Alleles) {
+    for (k in (Pop_Alleles+1):max(New_Queens$Allele_1)) {
+      New_Allele = data.frame(rep(0, length(New_Queens$Allele_1)))
+      Name = paste("Aus", k, sep="")
+      names(New_Allele) = Name
+      New_Queens = cbind(New_Queens, New_Allele)
+    }
+  }
+  
+  #Removes the "extra" queens that the large vector produced, or removes any queens that are impossible, ie, their two sex alleles are the same.
+  New_Queens <- New_Queens[New_Queens$Allele_1 != 0,]
+  New_Queens <- New_Queens[New_Queens$Allele_1 != New_Queens$Allele_2,]
+  
+  #Kills this if they're all dead
+  if (length(New_Queens$Position) == 0) {
+    print("Stop. Stop, they're already dead!")
+    break
+  } 
+  
+  return(New_Queens)
+}
